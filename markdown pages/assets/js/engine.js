fef0671b43ec0a438190f111546c0b56edb2ebd5
codlark <codlark@gmail.com>
2020-10-06 12:46:18 -0700

    Changed links, homepage title
    Changed the way links work, so they work right when opening links in new tabs. Overall they should be more robust.
    Also add a title to the home page.

--- markdown pages/assets/js/engine.js
+++ markdown pages/assets/js/engine.js
@@ -1,13 +1,3 @@
-function bind_links() {
-    $("a.md-link").on("click", load_and_render)
-}
-
-function load_and_render(e) {
-    var src = e.target.dataset.md
-    toc.navigate(src.slice(0,-9), {trigger: true})
-    return false
-    //suppress any other actions from happening
-}
 
 function main() {
     //prep backbone
@@ -20,9 +10,8 @@ function main() {
             $.get(file, function(text) {
                 $("#content").html(marked(text))
                 var title_re = /# (.*)/
-                title = text.match(/# (.*)/)[1]
+                title = text.match(title_re)[1]
                 document.title = title
-                bind_links()
             })
         }
     })
@@ -35,10 +24,10 @@ function main() {
     // holds the tags we want to modify
         link: function(href, title, text) {
         // <a> tag
-            if ((/m(ark)?d(own)?$/gi).test(href)) {
-                return `<a class="md-link" data-md="${href}" href="#">${text}</a>`
-                // an empty href is used while the markdown document is stored
-                // in the data-md attribute
+            var name = href.match(/(.*?)(\.markdown)/i)
+            console.log(name)
+            if (name != null) {
+                return `<a class="md-link" href="#${name[1]}">${text}</a>`
             } else {
                 return false
                 // return false to fallback to default
@@ -53,7 +42,6 @@ function main() {
     })
 
     $(document).ready(function() {
-        bind_links()
         Backbone.history.start({pushstate: true})
 
     })
